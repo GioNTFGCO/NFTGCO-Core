6c57db50d90942f61be6dde332e796a2c7b1ace2
GioNTFGCO <129079459+GioNTFGCO@users.noreply.github.com>
2023-05-23 19:22:46 -0400

    add scene addressable

--- Assets/Runtime/Helpers/SceneField/SceneLoaderController.cs
+++ Assets/Runtime/Helpers/SceneField/SceneLoaderController.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.SceneManagement;
+
 namespace SceneField.Core
 {
     public class SceneLoaderController : MonoBehaviour
     {
         [SerializeField] private SceneFieldConvertor _loadingScene;
         [SerializeField] private SceneFieldConvertor _levelScene;
+        [SerializeField] private string _levelKey;
         private AsyncOperation _loadingSceneAsync;
         public void StartLevel()
         {
@@ -26,6 +29,10 @@ namespace SceneField.Core
                 _loadingSceneAsync.allowSceneActivation = true;
             }
             SceneManager.LoadSceneAsync(levelScene);
+        }
+        public void LoadSceneAddressable()
+        {
+            Addressables.LoadSceneAsync(_levelKey);
         }
     }
 }
